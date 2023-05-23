ca979d01f4c1f2ac6d6cf53f5c6ecc8e1c6df5b4
Lorena Sabino Guedes <lorenasg1@gmail.com>
2023-05-23 11:34:01 +0000

    mudando a frase de novo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ fontFamily: 'sans-serif', lineHeight: 1.5 }}>
-      <p><strong>Te amo, Carla! ❤️</strong></p>
+      <p>o que será que tá rolando aqui?</p>
     </main>
   );
 }
