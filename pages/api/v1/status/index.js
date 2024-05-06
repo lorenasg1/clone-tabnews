7909f9237e39c6e1d6f44fa35590f1f647dcc025
lorenasg1 <lorenasg1@gmail.com>
2024-05-06 08:57:58 -0300

    finish /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,28 @@
 import database from "@/infra/database";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum");
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseInfo = await database.query({
+    text: `SELECT
+    current_setting('server_version') AS version,
+    current_setting('max_connections')::int AS max_connections,
+    (SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1) AS opened_connections;
+    `,
+    values: [databaseName],
+  });
+
+  const { version, max_connections, opened_connections } = databaseInfo.rows[0];
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version,
+        max_connections,
+        opened_connections,
+      },
+    },
+  });
 }
