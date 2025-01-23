f5196b73fc082e15eb7723ec3517073371f1cf5c
lorenasg1 <lorenasg1@gmail.com>
2025-01-23 08:34:55 -0300

    improve script test with retry and orchestrator

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,11 +1,9 @@
 import database from "infra/database";
-
-async function cleanDatabase() {
-  await database.query("drop schema public cascade; create schema public;");
-}
+import { waitForAllServices } from "@/tests/orchestrator";
 
 beforeAll(async () => {
-  await cleanDatabase();
+  await waitForAllServices();
+  await database.query("drop schema public cascade; create schema public;");
 });
 
 test("GET to /api/v1/migrations should return 200", async () => {
